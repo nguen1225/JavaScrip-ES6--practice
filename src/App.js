248e2b0cce1abd03e09985604713d24f2377f4c8
nguen1225 <Nguen1225@gmail.com>
2020-08-28 15:12:26 +0900

    カウントアプリ作成

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component{
-  render()
-  {
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
+const App = () => {
+	return <Counter />
+}
+
+class Counter extends Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+			value : 0
+		}
+	}
+
+	//インクリメントする関数
+	onIncrement = () => {
+		//setStateでstateの値を更新
+		this.setState({ value : this.state.value + 1});
+	}
+
+	//デクリメントする関数
+	onDecrement = () => {
+		//setStateでstateの値を更新
+		this.setState({ value : this.state.value - 1});
+	}
+
+	render()
+	{
+		return (
+			<div>
+				<div>
+					カウント値:{this.state.value}
+				</div>
+				<div>
+					<button type="button" onClick={this.onIncrement}>+</button>
+          			<button type="button" onClick={this.onDecrement}>-</button>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
